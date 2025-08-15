c50edb9c68a73007656cbfef425d946d5052bf1c
Yujioh <jioyu14@gmail.com>
2025-08-15 20:25:30 +0900

    first commit

---
+++ src/test/java/com/example/ildeurim/IldeurimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ildeurim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IldeurimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
