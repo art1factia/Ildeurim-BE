5cc21feb87d30eea389a31d14ee26393303d1c3e
art1factia <137972596+art1factia@users.noreply.github.com>
2025-08-19 09:11:44 +0900

    refactoring

--- src/main/java/com/example/Ildeurim/service/jobpost/JobPostService.java
+++ src/main/java/com/example/Ildeurim/service/jobpost/JobPostService.java
@@ -1,8 +1,8 @@
 package com.example.Ildeurim.service.jobpost;
 
 import com.example.Ildeurim.domain.JobPost;
-import com.example.Ildeurim.dto.jobpost.PostDetailResponse;
-import com.example.Ildeurim.dto.jobpost.SimplePostResponse;
+import com.example.Ildeurim.dto.jobpost.JobPostDetailRes;
+import com.example.Ildeurim.dto.jobpost.SimpleJobPostRes;
 import com.example.Ildeurim.repository.JobPostRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -16,15 +16,18 @@ public class JobPostService {
     private final JobPostRepository jobPostRepository;
 
     //공지글 전체 조회
-    public List<SimplePostResponse> getAllJobPost(){
-        List<JobPost> JobPostList =  jobPostRepository.findAll();
-        List<SimplePostResponse> SimplePostResponseList = JobPostList.stream().map(jobPost -> SimplePostResponse.of(jobPost)).toList();
+    public List<SimpleJobPostRes> getAllJobPost() {
+        List<JobPost> JobPostList = jobPostRepository.findAll();
+        List<SimpleJobPostRes> SimplePostResponseList = JobPostList.stream()
+                .map(jobPost -> SimpleJobPostRes.of(jobPost))
+                .toList();
         return SimplePostResponseList;
     }
 
     //공지글 단건 조회
-    public PostDetailResponse getJobPost(long id){
-        JobPost jobPost= jobPostRepository.findById(id).orElseThrow(()-> new EntityNotFoundException("해당 ID의 게시글을 찾을 수 없습니다. ID: "+id));
+    public JobPostDetailRes getJobPost(long id) {
+        JobPost jobPost = jobPostRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("해당 ID의 게시글을 찾을 수 없습니다. ID: " + id));
 
 
     }
