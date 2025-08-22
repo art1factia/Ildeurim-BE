e13b5fa8b7947b25f240fe2229968813e917e172
art1factia <137972596+art1factia@users.noreply.github.com>
2025-08-22 18:10:36 +0900

    job | career domain skeleton code

---
+++ src/main/java/com/example/Ildeurim/dto/career/CareerRes.java
@@ -0,0 +1,4 @@
+package com.example.Ildeurim.dto.career;
+
+public record CareerRes() {
+}
---
+++ src/main/java/com/example/Ildeurim/service/CareerService.java
@@ -0,0 +1,12 @@
+package com.example.Ildeurim.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CareerService {
+    //TODO: create 메서드 작성
+    //TODO: update 메서드 작성
+    //TODO: delete 메서드 작성
+}
