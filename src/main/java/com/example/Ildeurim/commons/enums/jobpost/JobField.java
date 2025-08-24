19b1e93ba333ab71acb75ac9466fe5f5cfc66bb7
art1factia <137972596+art1factia@users.noreply.github.com>
2025-08-25 00:21:18 +0900

    jobField enum update

--- src/main/java/com/example/Ildeurim/commons/enums/jobpost/JobField.java
+++ src/main/java/com/example/Ildeurim/commons/enums/jobpost/JobField.java
@@ -8,16 +8,16 @@ public enum JobField {
     AGRICULTURE("농사,원예,어업"),
     MANUFACTURING("목공,공예,제조"),
     DELIVERY("운전,배달"),
-    MECHANIC_REPAIR("기계,금속 제작, 수리"),
-    CLEANING("청소, 미화"),
-    CONSTRUCTION("건설, 시설 관리"),
-    ELECTRONICS_REPAIR("전기, 전자 수리"),
+    MECHANIC_REPAIR("기계,금속 제작,수리"),
+    CLEANING("청소,미화"),
+    CONSTRUCTION("건설,시설 관리"),
+    ELECTRONICS_REPAIR("전기,전자 수리"),
     CARE("돌봄"),
     SALES("판매"),
     FOOD_SERVICE("음식,서비스"),
-    CULTURE_RESEARCH("문화, 연구, 기술"),
-    OFFICE_FINANCE("사무, 금융"),
-    FOOD_CLOTHE_ENV("식품·옷·환경 가공"),
+    CULTURE_RESEARCH("문화,연구,기술"),
+    OFFICE_FINANCE("사무,금융"),
+    FOOD_CLOTHE_ENV("식품,옷,환경 가공"),
     OTHER("기타");
 
     private final String label;
